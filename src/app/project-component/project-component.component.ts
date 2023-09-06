10ccb9c86a468f35996444d3b5241b00bd8fa3a3
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-09-06 15:40:13 +0200

    add new project + fog

--- src/app/project-component/project-component.component.ts
+++ src/app/project-component/project-component.component.ts
@@ -9,11 +9,13 @@ export class ProjectComponentComponent {
   projectImages: Array<string> = [
     'assets/img/join.png',
     'assets/img/sharkie.png',
+    'assets/img/Simple-CRM.png',
   ];
-  projectTitle: Array<string> = ['Join', 'Sharkie'];
-  projectStack: Array<string> = ['Angular | TypeScript | HTML | CSS | Firebase ', 'JavaScript | HTML | CSS']
+  projectTitle: Array<string> = ['Join', 'Sharkie','Simple CRM' ];
+  projectStack: Array<string> = ['Angular | TypeScript | HTML | CSS | Firebase ', 'JavaScript | HTML | CSS', 'Angular | Firebase']
   projectDescription: Array<string> = [
     'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-    'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.'
+    'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+    'A very Simple Customer Relationship Management system working with CRUD functionality.'
   ];
 }
