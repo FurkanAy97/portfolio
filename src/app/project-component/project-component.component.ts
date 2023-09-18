7fd10293251afe1eebc48cc56209301ae57802a0
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-09-18 21:38:00 +0200

    project-comp responsive WIP

--- src/app/project-component/project-component.component.ts
+++ src/app/project-component/project-component.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-project-component',
   templateUrl: './project-component.component.html',
   styleUrls: ['./project-component.component.scss'],
 })
-export class ProjectComponentComponent {
+export class ProjectComponentComponent extends AppComponent {
   projectImages: Array<string> = [
     'assets/img/join.png',
     'assets/img/sharkie.png',
     'assets/img/Simple-CRM.png',
   ];
-  projectTitle: Array<string> = ['Join', 'Sharkie','Simple CRM' ];
-  projectStack: Array<string> = ['Angular | TypeScript | HTML | CSS | Firebase ', 'JavaScript | HTML | CSS', 'Angular | Firebase']
+  projectTitle: Array<string> = ['Join', 'Sharkie', 'Simple CRM'];
+  projectStack: Array<string> = [
+    'Angular | TypeScript | HTML | CSS | Firebase ',
+    'JavaScript | HTML | CSS',
+    'Angular | Firebase',
+  ];
   projectDescription: Array<string> = [
     'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
     'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
-    'A very Simple Customer Relationship Management system working with CRUD functionality.'
+    'A very Simple Customer Relationship Management system working with CRUD functionality.',
   ];
+
 }
