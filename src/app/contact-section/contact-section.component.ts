d4ba7c038bcc3ee7209cf0cd997027d49d5ad248
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-10-04 16:59:40 +0200

    Contact formvalidation WIP

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -4,32 +4,30 @@ import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-contact-section',
   templateUrl: './contact-section.component.html',
-  styleUrls: ['./contact-section.component.scss']
+  styleUrls: ['./contact-section.component.scss'],
 })
-export class ContactSectionComponent extends AppComponent  {
-
-  arrowSrc = 'assets/img/go-up-button.png'
-  arrowSrcHover = 'assets/img/gp-up-button-blue.png'
+export class ContactSectionComponent extends AppComponent {
+  arrowSrc = 'assets/img/go-up-button.png';
+  arrowSrcHover = 'assets/img/gp-up-button-blue.png';
   isHovered = false;
-  isSuccess: boolean = false
-  isFail: boolean = false
-  @ViewChild('myForm') myForm!: ElementRef
-  @ViewChild('nameField')nameField!: ElementRef
-  @ViewChild('emailField') emailField!: ElementRef
-  @ViewChild('messageField') messageField!: ElementRef
-  
-  
-   formData = {
+  isSuccess: boolean = false;
+  isFail: boolean = false;
+  isSentSuccessfully: boolean = false;
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+
+  formData = {
     name: '',
     email: '',
-    message: ''
+    message: '',
   };
-  agreeToPolicy: boolean = false
+  agreeToPolicy: boolean = false;
 
-
-  async sendMail(){
-    let nameField = this.nameField.nativeElement
-    let emailField = this.emailField.nativeElement
+  async sendMail() {
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
     let messageField = this.messageField.nativeElement;
 
     nameField.disabled = true;
@@ -37,20 +35,31 @@ export class ContactSectionComponent extends AppComponent  {
     messageField.disabled = true;
     /* Animation */
     let formData = new FormData();
-    formData.append('name', nameField.value)
-    formData.append('emailField', emailField.value)
-    formData.append('messageField', messageField.value)
+    formData.append('name', nameField.value);
+    formData.append('email', emailField.value);
+    formData.append('message', messageField.value);
 
-    await fetch('https://furkan-ayhan.developerakademie.net/send_mail/send_mail.php',  
-    {
-      method: 'POST',
-      body: formData
-    })
-    .then()
-    /* Text anzeigen Nachicht gesendet */
+    await fetch(
+      'https://furkan-ayhan.developerakademie.net/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          this.isSentSuccessfully = true;
+        } else {
+          this.isSentSuccessfully = false;
+          console.error('Failed to send the mail.');
+        }
+      })
+      .catch((error) => {
+        this.isSentSuccessfully = false;
+        console.error('An error occurred:', error);
+      });
     nameField.disabled = false;
     emailField.disabled = false;
     messageField.disabled = false;
   }
-
 }
