f99f0e7c16cbf7a94d279c2e196c87c62a1cdd4c
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-10-20 16:34:19 +0200

    Established Parent-Child Class Relationship and Passed Dependency to Parent Constructor on header links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { Router, NavigationEnd  } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
@@ -8,6 +11,23 @@ import { AppComponent } from '../app.component';
 })
 export class HeaderComponent extends AppComponent {
   burgerMenuVisible: boolean = false;
+  isHomePage: boolean = false
+
+
+  constructor(viewportScroller: ViewportScroller, private router: Router) {
+    super(viewportScroller);
+    // Subscribe to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current route is not '/imprint'
+        if (event.url == '/') {
+          this.isHomePage = true;
+        } else {
+          this.isHomePage = false;
+        }
+      }
+    });
+  }
 
   toggleBurgerMenu() {
     this.burgerMenuVisible = !this.burgerMenuVisible;
