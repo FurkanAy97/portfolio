1b2e351e5bd10544b458168d90e1be48c1a0ec85
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-10-23 19:08:09 +0200

    add LocationStrategy and target="_blank"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'imprint', component: ImprintComponent },
-  { path: 'privacy-policy', component: PrivacyPolicyComponent }
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollOffset: [0,150]})],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
